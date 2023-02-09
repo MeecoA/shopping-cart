88c07e61860eca0791ede1141e1605cbc584351c
Coa <meecorcll@gmail.com>
2023-02-10 07:48:46 +0800

    fixed, router issue

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 const RouteSwitch = () => {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter basename="/">
         <Nav></Nav>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 };
